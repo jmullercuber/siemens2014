c72e7447b4fa5a40e02e05076f6acd65f09e43ef
Sam <Sam@Sam_Computer>
2014-08-08 07:56:39 -0600

    Added commenting to game.

--- siemens2014/src/org/samjoey/model/Game.java
+++ siemens2014/src/org/samjoey/model/Game.java
@@ -25,21 +25,28 @@ import java.util.Iterator;
  */
 public class Game {
 
-    private ArrayList<Board> turns;
+    private ArrayList<Board> turns; //The list of boards
     private int winner; //0 for draws, 1 for white, 2 for black
     private String winType; //DRAW, FORFEIT, CHECKMATE, among others
-    private double whiteTime;
-    private double blackTime;
-    private int plyCount;
-    private int timeAllowedPerPlayer;
-    private HashMap<String, ArrayList<Integer>> vars;
-    private Iterator iterator;
+    private double whiteTime; //How much time white took
+    private double blackTime; //How much time black took
+    private int plyCount; //The number of half-moves taken
+    private int timeAllowedPerPlayer; //How much time was allowed per player
+    private HashMap<String, ArrayList<Integer>> vars; //The variables calculated
+    private Iterator iterator; //The iterator over the boards
 
+    /**
+     * Default constructor, initializes list and map
+     */
     public Game() {
         turns = new ArrayList<>();
         vars = new HashMap<>();
     }
 
+    /**
+     * Initializes the iterator if it needs to and gets the next board.
+     * @return the next board in the game.
+     */
     public Board next() {
         if (iterator == null) {
             iterator = turns.iterator();
@@ -47,18 +54,22 @@ public class Game {
         return (Board) iterator.next();
     }
 
+    //Restart the iterator.
     public void restart() {
         iterator = turns.iterator();
     }
 
+    //Return all variable data
     public HashMap<String, ArrayList<Integer>> getVarData() {
         return vars;
     }
 
+    //Get a specific variable
     public ArrayList<Integer> getVar(String var) {
         return vars.get(var);
     }
 
+    //Add data for a variable. Also handles adding the variable if it is not already mapped
     public void addVariable(String name, Integer value) {
         if (vars.containsKey(name)) {
             vars.get(name).add(value);
@@ -68,66 +79,72 @@ public class Game {
         }
     }
     
+    //Get all boards for this game
     public ArrayList<Board> getAllBoards(){
         return this.turns;
     }
     
+    //Add a board to this game
     public void addBoard(Board b){
         this.turns.add(b);
     }
 
+    //Get the winner of this game
     public int getWinner() {
         return winner;
     }
 
+    //Set the winner of this game
     public void setWinner(int winner) {
         this.winner = winner;
     }
 
+    //Get the way this game was won
     public String getWinType() {
         return winType;
     }
 
+    //Set the way this game was won
     public void setWinType(String winType) {
         this.winType = winType;
     }
 
+    //Get the amount time white took
     public double getWhiteTime() {
         return whiteTime;
     }
 
+    //Set the amount of time white took
     public void setWhiteTime(double whiteTime) {
         this.whiteTime = whiteTime;
     }
 
+    //Get the amount of time black took
     public double getBlackTime() {
         return blackTime;
     }
 
+    //Set the amount of time black took
     public void setBlackTime(double blackTime) {
         this.blackTime = blackTime;
     }
 
-    public Iterator getIterator() {
-        return iterator;
-    }
-
-    public void setIterator(Iterator iterator) {
-        this.iterator = iterator;
-    }
-
+    //Get how much time is allowed for each player
     public int getTimeAllowedPerPlayer() {
         return timeAllowedPerPlayer;
     }
 
+    //Set how much time is allowed for each player
     public void setTimeAllowedPerPlayer(int timeAllowedPerPlayer) {
         this.timeAllowedPerPlayer = timeAllowedPerPlayer;
     }
 
+    //Get the ply count
     public int getPlyCount() {
         return plyCount;
     }
 
+    //Set the ply count
     public void setPlyCount(int plyCount) {
         this.plyCount = plyCount;
     }

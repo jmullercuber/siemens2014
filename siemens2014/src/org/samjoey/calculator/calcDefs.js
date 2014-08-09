1023e3a886463aed62ba604e5e2823965e39214a
joey <jmullercuber@gmail.com>
2014-08-09 15:44:39 -0600

    Driver started, GameLooper almost done, templates for rest Got a hang on doing this java/javascript stuff. Driver is structure is VERY basic, but enough to get started once edited. The GameLooper has a good start based on its UML Calculator is next to do

---
+++ siemens2014/src/org/samjoey/calculator/calcDefs.js
@@ -0,0 +1,11 @@
+/*
+ * Here place definitions for various pre-made Calculators
+ * These names will be imported to the driver, and will
+ * have local meaning there.
+ * Purpuse: To not culter up that precious driver file 
+ */
+ 
+ import ('Calculator.js');
+ 
+ var WeightedCenter = new Calculator(
+);
---
+++ siemens2014/src/org/samjoey/gameLooper/GameLooper.js
@@ -0,0 +1,59 @@
+function GameLooper(initialGame, calcList) {
+	/*
+	 To do:
+	 research how rhino deals with undefined vs null
+	 //figure out type of error open() should throw
+	 	nope, from MDN I understand how to throw Error
+	 implement calculate()
+	 implement read() or move functionality to different class
+	*/
+	
+	//----------------------Attributes----------------------
+	
+	// reference variable to the current "Game" under analysis
+	this.currentGame = null;
+	if (arguments.length > 0) {
+		this.currentGame = initialGame;
+	}
+	
+	// keep track of which calculators apply to this GL
+	this.calcs = [];
+	if (arguments.length > 1) {
+		// only the driver should really be doing this, so don't worry about Java arrays at all
+		this.calcs = calcList.splice();	// Array.splice() is a copy
+	}
+	
+	//------------------------Methods------------------------
+	
+	// Open a Game so that we can start dealing with it. Throw an {ErrorNameHere} error if another Game is already open.
+	this.open = function(game) {
+		if (currentGame != null) {
+			throw new Error("GameLooper - AlreadyInUse");
+		}
+		this.currentGame = game;
+	}
+
+	// Close the currentGame so another Game can be used
+	this.close = function() {
+		if (currentGame == null) {
+			//already closed/never opened
+			throw new Error("GameLooper - AlredyClosed");
+		}
+		currentGame = null;
+	}
+	
+	// addCalculator() makes each GameLooper different. Calculates different stuff
+	this.addCalculator = function(oneCalc) {
+		this.calcs.push(oneCalc);
+	}
+	
+	// this is like the most important part
+	// calculate() takes the current game
+	// and edits it's internal HashMap
+	// by appending data collected for each Calculator
+	this.calculate = function() {
+		/*
+		   <implementation here>
+		*/
+	}
+}
